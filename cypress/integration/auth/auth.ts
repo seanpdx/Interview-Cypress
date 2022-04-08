8fed823b91d9338e365e8747cec94db5b3c5ff0b
Sean Bradford <bradfordphonic@gmail.com>
2022-04-08 13:06:23 -0700

    wrong password test complete

--- cypress/integration/auth/auth.ts
+++ cypress/integration/auth/auth.ts
@@ -6,5 +6,9 @@ Then("I am notified that I am not authorized to log in", () => {
  
   // Continue button Disabled
   // Login error message shown
+
+  cy.get('button[data-cy="submit"]').should('be.disabled');
+  cy.contains('Not authorized').should('be.visible');
+
 });
 
